c3e33130baf675505a66e0841863742aac54242d
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2022-02-14 06:15:17 -0800

    changes made

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const path = require('path')
-const PORT = 3000 || process.env.PORT
+const PORT = 9000 || process.env.PORT
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
